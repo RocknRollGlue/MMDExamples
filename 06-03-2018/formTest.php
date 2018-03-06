6955bbce0c3b38f27a9cd456eff7174bd603b98b
M. Nyrup <mathiasnyrup@protonmail.com>
2018-03-06 13:16:43 +0100

    Lavede comments

--- 06-03-2018/formTest.php
+++ 06-03-2018/formTest.php
@@ -9,12 +9,12 @@
 </form>
 <?php
 
-    $theUser = $_POST['user'];
-    if($theUser != "" || $theUser != null){
-        echo "Hello ". $theUser."!";
+    $theUser = $_POST['user']; //Vi bruger $_POST super global variablen til at få fat i input med navnet 'user'. Variablen er nu inputtets text
+    if($theUser != "" || $theUser != null){ //Vi laver et kontrol om inputtet er tomt eller ikke eksisterende!
+        echo "Hello ". $theUser."!"; // Vi siger hej til brugeren!
     }
 
-    echo "<br/>"
+    echo "<br/>"; // Til sidst laver vi break til at give plads hvis der skal skrives mere.
 ?>
 </body>
 </html>
